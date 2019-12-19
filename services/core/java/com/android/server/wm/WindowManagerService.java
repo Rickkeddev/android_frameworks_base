373ca91f0804226eaa97337a3b3b059dc7ec0a05
Hưng Phan <phandinhhungvp2001@gmail.com>
2019-12-19 17:29:05 +0700

    Revert "Don't report existing menu key if it was remapped. [2/3]"
    This reverts commit 83a1659cf4459f104ce3f7aeaf0bd37b17eb23cf.

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5818,11 +5818,6 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
-    public boolean hasPermanentMenuKey() {
-        return mPolicy.hasPermanentMenuKey();
-    }
-
-    @Override
     public void sendCustomAction(Intent intent) {
         mPolicy.sendCustomAction(intent);
     }
